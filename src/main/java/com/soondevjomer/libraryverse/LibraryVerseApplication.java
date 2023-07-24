84974b5437419957f3a09884fd2734d8e3c6034b
soondevjomer <jomerverola99@gmail.com>
2023-07-24 13:34:31 +0800

    Library-Verse first commit

---
+++ src/main/java/com/soondevjomer/libraryverse/LibraryVerseApplication.java
@@ -0,0 +1,13 @@
+package com.soondevjomer.libraryverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryVerseApplication.class, args);
+	}
+
+}
